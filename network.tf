f333855bcfadf4d07b6036b042b98dac47649ae5
Sai Jithendra Gonjisetty <139713856+Sai-Jithendra-Gonji@users.noreply.github.com>
2025-11-24 22:28:22 +0000

    Add Azure network resources and security groups

---
+++ network.tf
@@ -0,0 +1,83 @@
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = var.vnet_address_space
+  tags                = var.tags
+}
+
+# Management Subnet
+resource "azurerm_subnet" "management" {
+  name                 = var.management_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.management_subnet_prefix
+}
+
+# Runtime Subnet
+resource "azurerm_subnet" "runtime" {
+  name                 = var.runtime_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.runtime_subnet_prefix
+}
+
+# NSG for Management Subnet
+resource "azurerm_network_security_group" "management" {
+  name                = "nsg-${var.management_subnet_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
+
+  security_rule {
+    name                       = "AllowAKSInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+# NSG for Runtime Subnet
+resource "azurerm_network_security_group" "runtime" {
+  name                = "nsg-${var.runtime_subnet_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
+
+  security_rule {
+    name                       = "AllowAKSInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Management Subnet
+resource "azurerm_subnet_network_security_group_association" "management" {
+  subnet_id                 = azurerm_subnet.management.id
+  network_security_group_id = azurerm_network_security_group.management.id
+}
+
+# Associate NSG with Runtime Subnet
+resource "azurerm_subnet_network_security_group_association" "runtime" {
+  subnet_id                 = azurerm_subnet.runtime.id
+  network_security_group_id = azurerm_network_security_group.runtime.id
+}
